84a86ffba506cc86105817030ac7ce5a26f81ed8
JustGaleo <leonardogarciamartinez@gmail.com>
2023-04-10 17:47:35 -0600

    I've added SQL logic

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ from pydantic import BaseModel, Field
 from typing import Optional, List
 from jwt_manager import create_token, validate_token
 from fastapi.security import HTTPBearer
+from config.database import Session, engine, Base 
+from models.movie import Movie as MovieModel
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 app.title = 'My app with FastAPI'
 app.version = '0.0.1'
+
+Base.metadata.create_all(bind=engine)
 
 class JWTBearer(HTTPBearer):
     async def __call__(self, request: Request):
@@ -24,7 +29,7 @@ class Movie(BaseModel):
     id: Optional[int] = None
     title: str = Field(min_length=5, max_length=100)
     overview: str = Field(min_length=5, max_length=100)
-    year: str = Field(min_length=5)
+    year: int = Field(le=2023)
     rating: float = Field(ge=1, le=10)
     category: str = Field(min_length=5, max_length=15)
 
@@ -51,45 +56,89 @@ def login(user: User):
 
 @app.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
 def getMovies() -> List[Movie]:
-    if len(movies) == 0:
-        raise HTTPException(status_code=404, detail="There aren't movies")
+    try:
+        db = Session()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     else:
-        return JSONResponse(content=movies, status_code=200)
+        result = db.query(MovieModel).all()
+        if not result:
+            raise HTTPException(status_code=404, detail="Movie not found")
+        else: 
+            return JSONResponse(content=jsonable_encoder(result), status_code=200)
 
 @app.get('/movies/{id}', tags=['movies'], response_model=Movie, status_code=200)
 def getMoviesById(id: int = Path(ge=1, le=2000)) -> Movie:
-    data = [movie for movie in movies if movie['id'] == id]
-    if len(data) == 0:
-        raise HTTPException(status_code=404, detail="Movie not found")
+    try:
+        db = Session()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     else:
-        return JSONResponse(content=data, status_code=200)
+        result = db.query(MovieModel).filter(MovieModel.id == id).first()
+        if not result:
+            raise HTTPException(status_code=404, detail="Movie not found")
+        else: 
+            return JSONResponse(content=jsonable_encoder(result), status_code=200)
 
 #get request with query params
 @app.get('/movies/', tags=['movies'], response_model=List[Movie], status_code=200)
 def getMoviesByCategory(category: str = Query(min_length=10, max_length=15)) -> List[Movie]:
-    data = [movie for movie in movies if movie['category'] == category]
-    if len(data) == 0:
-        raise HTTPException(status_code=404, detail="Movie not found")
+    try:
+        db = Session()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     else:
-        return JSONResponse(content=data, status_code=200)
+        result = db.query(MovieModel).filter(MovieModel.category == category).all()
+        if not result:
+            raise HTTPException(status_code=404, detail="Movie not found")
+        else: 
+            return JSONResponse(content=jsonable_encoder(result), status_code=200)
 
-@app.post('/movie', tags =['movies'], response_model=List[Movie], status_code=201)
-def addMovie(movie: Movie) -> List[Movie]:
-    movies.append(movie)
-    return JSONResponse(content=movies, status_code=201)
+@app.post('/movie', tags =['movies'], response_model=dict, status_code=201)
+async def addMovie(movie: Movie) -> dict:
+    try:
+        db = Session()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    else:
+        new_movie = MovieModel(**movie.dict())
+        db.add(new_movie)
+        db.commit()
+        return JSONResponse(status_code=201, 
+                            content={"message": "Upload Successful",
+                                     "details": movie.dict()})
 
 @app.put('/movie/{id}', tags=['movies'], response_model=List[Movie], status_code=200)
 def modifyMovie(movie: Movie, id: int = Path(ge=1, le=2000)) -> List[Movie]:
-    for item in movies:
-        if item["id"] == id:
-            item.update(movie)
-    return JSONResponse(content=movies, status_code=200)
-
-@app.delete('/movies/{id}', tags=['movies'], response_model=List[Movie], status_code=200)
-def deleteMovie(id: int = Path(ge=1, le=2000)) -> List[Movie]:
-    movies_by_id = list(filter(lambda x: x['id'] == id , movies))
-    if(len(movies_by_id) > 0):
-        movies.remove(movies_by_id[-1])
-        return JSONResponse(content=movies, status_code=200)
+    try:
+        db = Session()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     else:
-        raise HTTPException(status_code=404, detail="Movie not found")
+        result = db.query(MovieModel).filter(MovieModel.id == id).first()
+        if not result:
+            raise HTTPException(status_code=404, detail="Movie not found")
+        else: 
+            result.title = movie.title
+            result.category = movie.overview
+            result.overview = movie.overview
+            result.rating = movie.rating
+            result.year = movie.year  
+            db.commit()
+            return JSONResponse(status_code=201, content={"message": "Updated done"})
+
+
+@app.delete('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
+def deleteMovie(id: int = Path(ge=1, le=2000)) -> dict:
+    try:
+        db = Session()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    else:
+        result = db.query(MovieModel).filter(MovieModel.id == id).first()
+        if not result:
+            raise HTTPException(status_code=404, detail="Movie not found")
+        else: 
+            db.delete(result)
+            db.commit()
+            return JSONResponse(status_code=201, content={"message": "Delete successful"})
